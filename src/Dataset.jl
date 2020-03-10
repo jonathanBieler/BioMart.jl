5d87578d579d8a2163415bcd9e5ec15555eef1e2
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2020-03-10 09:55:18 +0100

    added list_attributes

--- src/Dataset.jl
+++ src/Dataset.jl
@@ -9,7 +9,7 @@ Dataset(dataset::String) = Dataset(dataset,"","")
 @memoize function datasets(database::String)
 
     url = string(BioMart.BIOMART_URL, "?type=datasets&requestid=biomaRt&mart=$(database)")
-    r = HTTP.get(url)
+    r = HTTP.get(url, cookies=true)
     
     out = Dataset[]
 
@@ -39,3 +39,47 @@ Examples :
 
 """ 
 datasets(database::Database) = datasets(database.name)
+
+"""
+    BioMart.list_attributes(dataset::Dataset)
+    BioMart.list_attributes(dataset::String)
+
+List the attributes available for a given dataset.
+
+Examples : 
+
+    BioMart.list_attributes("hsapiens_gene_ensembl")
+""" 
+@memoize function list_attributes(dataset::String)
+
+    url = string(BioMart.BIOMART_URL, "?type=configuration&dataset=$(dataset)")
+    r = HTTP.get(url, cookies=true)
+    data = String(r.body)
+
+    xdoc = parse_string(data)
+    xroot = root(xdoc)
+
+    out = collect_elems(xroot)
+    DataFrame(out)
+end
+
+list_attributes(dataset::Dataset) = list_attributes(dataset.dataset)
+
+"""go recursively over the XML and collect `AttributeDescription`s"""
+function collect_elems(node, out) 
+
+    if name(node) == "AttributeDescription"
+        el = XMLElement(node)
+        push!(out, (
+                internalName = attribute(el, "internalName"),
+                displayName  = attribute(el, "displayName"),
+            )
+        )
+    else
+        for c in child_nodes(node)
+            collect_elems(c, out)
+        end
+    end
+    out
+end 
+collect_elems(node) = collect_elems(node, [])
