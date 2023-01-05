d70828ed76a79684b391ccc40f72302fa6470ae8
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2023-01-05 16:10:23 +0100

    update doc make

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,22 @@
 using Documenter
 using BioMart
 
-makedocs(
-    sitename = "BioMart",
-    format = Documenter.HTML(),
-    modules = [BioMart]
+makedocs(;
+    modules=[BioMart],
+    authors="Jonathan Bieler <jonathan.bieler@alumni.epfl.ch> and contributors",
+    repo="https://github.com/jonathanBieler/BioMart.jl/blob/{commit}{path}#{line}",
+    sitename="BioMart.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://jonathanBieler.github.io/BioMart.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/jonathanBieler/BioMart.jl",
+    devbranch="main",
 )
